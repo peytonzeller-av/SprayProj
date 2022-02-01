e31ad26068191ab2426db8fcdf71a40bc9fc2d22
Peyton Zeller-Av <pzellera@gmail.com>
2022-01-31 20:44:47 -0700

    Added List Grouping per grade

--- ProblemList.js
+++ ProblemList.js
@@ -1,38 +1,65 @@
 import React, { useState, useEffect } from "react";
 import { StyleSheet, Text, View, FlatList } from "react-native";
 import { useNavigation } from "@react-navigation/native";
+import { List } from "react-native-paper";
 
 const ProblemList = () => {
-  const [myProblems, setMyProblems] = useState([]);
+  const navigation = useNavigation();
+  const [myProblems, setMyProblems] = useState([
+    // TODO: Mock Data
+    { key: "Problem1", grade: 1, createdOn: "01-30-2022" },
+    { key: "Problem2", grade: 3, createdOn: "01-30-2022" },
+    { key: "Problem3", grade: 5, createdOn: "01-30-2022" },
+    { key: "Problem4", grade: 2, createdOn: "01-30-2022" },
+    { key: "Problem5", grade: 8, createdOn: "01-30-2022" },
+    { key: "Problem6", grade: 7, createdOn: "01-30-2022" },
+    { key: "Problem7", grade: 6, createdOn: "01-30-2022" },
+    { key: "Problem8", grade: 6, createdOn: "01-30-2022" },
+    { key: "Problem9", grade: 4, createdOn: "01-30-2022" },
+    { key: "Problem10", grade: 3, createdOn: "01-30-2022" },
+  ]);
 
-  useEffect(() => {
-    const problems = fetch("10.0.0.233:5000/problems") // TODO - Error is here, may be due me connected to ethernet instead of a wifi network
-      .then((res) => console.log(res))
-      .catch((e) => console.log("new error", e));
-  }, []);
+  const grades = myProblems.reduce((acc, curr) => {
+    acc.add(curr.grade);
+    return acc;
+  }, new Set());
+
+  const getProblemsForGrade = (grade) =>
+    myProblems.filter((problem) => grade === problem.grade);
+
+  // TODO - Wait for wifi issues to get resolved before testing w/ express
+  // useEffect(() => {
+  //   const problems = fetch("10.0.0.233:5000/problems") // TODO - Error is here, may be due me connected to ethernet instead of a wifi network
+  //     .then((res) => console.log(res))
+  //     .catch((e) => console.log("new error", e));
+  // }, []);
 
   return (
-    <FlatList
-      data={[]}
-      renderItem={({ item }) => (
-        <Text
-          onPress={() =>
-            navigation.navigate("ProblemView", {
-              name: item.key,
-              grade: item.grade,
-            })
-          }
-          style={{
-            padding: 10,
-            fontSize: 18,
-            height: 44,
-          }}
-        >
-          {item.key}
-        </Text>
-      )}
-    />
+    <List.AccordionGroup>
+      {Array.from(grades).map((grade) => (
+        <List.Accordion title={`V${grade}`} id={grade}>
+          {getProblemsForGrade(grade).map((problem) => (
+            <List.Item
+              title={problem.key}
+              onPress={() =>
+                navigation.navigate("ProblemView", {
+                  name: problem.key,
+                  grade: problem.grade,
+                })
+              }
+            />
+          ))}
+        </List.Accordion>
+      ))}
+    </List.AccordionGroup>
   );
 };
+
+const styles = StyleSheet.create({
+  textGrade: {
+    marginRight: 6,
+    fontWeight: "bold",
+  },
+});
 
 export default ProblemList;
